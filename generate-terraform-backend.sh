39a0183370e27f6cc354235a75a5f5b2b6eec6e4
Cho seokgyu <ezaurum@gmail.com>
2025-10-05 15:09:13 +0900

    Update generate-terraform-backend.sh

--- generate-terraform-backend.sh
+++ generate-terraform-backend.sh
@@ -21,9 +21,21 @@ terraform {
     dynamodb_table = "$DDB"
     encrypt        = true
     kms_key_id     = "$KMS"
-    role_arn       = "$ROLE"
   }
 }
 EOF
 
 echo "backend.tf 파일이 생성되었습니다."
+
+# provider.tf 생성 (assume_role 포함)
+cat > provider.tf <<EOF
+provider "aws" {
+  region = "$REGION"
+
+  assume_role {
+    role_arn = "$ROLE_ARN"
+  }
+}
+EOF
+
+echo "provider.tf 파일이 생성되었습니다."
